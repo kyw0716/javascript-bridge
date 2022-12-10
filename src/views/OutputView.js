03defa6bc76f4337d35af0b00cb2d18a032fd79f
kyw0716 <kyw0716@naver.com>
2022-12-11 00:04:10 +0900

    feat: 현재 위치 출력 기능

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,3 +1,4 @@
+const { Console } = require("@woowacourse/mission-utils");
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -7,7 +8,24 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(map) {
+    const upMap = this.makeUpMap(map);
+    const downMap = this.makeDownMap(map);
+
+    Console.print(`[${upMap.join("|")}]`);
+    Console.print(`[${downMap.join("|")}]`);
+  },
+
+  printWrongMap(map) {
+    const [upMap, downMap] = this.makeWrongMap(
+      map[map.length - 1],
+      this.makeUpMap(map),
+      this.makeDownMap(map)
+    );
+
+    Console.print(`[${upMap.join("|")}]`);
+    Console.print(`[${downMap.join("|")}]`);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
@@ -15,6 +33,39 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult() {},
+
+  makeUpMap(map) {
+    const upMap = [];
+
+    for (let i = 0; i < map.length; i++) {
+      if (map[i] === "U") upMap.push(" O ");
+      if (map[i] === "D") upMap.push("   ");
+    }
+
+    return upMap;
+  },
+
+  makeDownMap(map) {
+    const downMap = [];
+
+    for (let i = 0; i < map.length; i++) {
+      if (map[i] === "D") downMap.push(" O ");
+      if (map[i] === "U") downMap.push("   ");
+    }
+
+    return downMap;
+  },
+
+  makeWrongMap(lastDirection, upMap, downMap) {
+    if (lastDirection === "U") {
+      upMap.pop();
+      upMap.push(" X ");
+      return [upMap, downMap];
+    }
+    downMap.pop();
+    downMap.push(" X ");
+    return [upMap, downMap];
+  },
 };
 
 module.exports = OutputView;
